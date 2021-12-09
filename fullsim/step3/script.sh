d325178d5a7969b0ea83103cd65f6d5ee0600db7
Gustavo <diemort@gmail.com>
2021-12-09 10:32:29 +0100

    adding files for step3

---
+++ fullsim/step3/script.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc900
+cmssw=$4
+scram project $cmssw
+cd $cmssw/src/
+eval `scramv1 runtime -sh`
+scram b -j8
+cd -
+if [ ! -d "${6}/out"]; then
+    mkdir ${6}/out
+fi
+if [ ! -d "${6}/err"]; then
+    mkdir ${6}/err
+fi
+if [ ! -d "${6}/log"]; then
+    mkdir ${6}/log
+fi 
+infile=step2_${1}.root
+outfile=${3}_${1}.root
+sed -i "s/xinput/$infile/g" $2
+sed -i "s/xfileout/$outfile/g" $2
+sed -i "s/xseed/$1/g" $2
+cmsRun ${2}
+ppseos=/eos/cms/store/group/phys_pps/sim-validation/full
+if [ ! -d "$ppseos/${3}" ]; then
+    mkdir $ppseos/${3}
+fi
+rsync -avPz $outfile $ppseos/${3}/$outfile
+rm -rf $outfile
