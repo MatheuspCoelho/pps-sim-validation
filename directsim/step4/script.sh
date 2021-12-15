937204304b0f66eeeb2c152a678eb04f652d8640
Gustavo <diemort@gmail.com>
2021-12-15 11:49:45 +0100

    adding condor files for step4 of direct sim

---
+++ directsim/step4/script.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc900
+cmssw=$4
+scram project $cmssw
+cd $cmssw/src/
+eval `scramv1 runtime -sh`
+git cms-init
+git cms-addpkg Validation/CTPPS
+scram b -j8
+cd -
+#if [ ! -d "${6}/out"]; then
+#    mkdir ${6}/out
+#fi
+#if [ ! -d "${6}/err"]; then
+#    mkdir ${6}/err
+#fi
+#if [ ! -d "${6}/log"]; then
+#    mkdir ${6}/log
+#fi 
+ppseos=/eos/cms/store/group/phys_pps/sim-validation/directsim
+infile=${ppseos}/step3/step3_${1}.root
+outfile=${3}_${1}.root
+sed -i "s?xinput?$infile?" $2
+sed -i "s/xfileout/$outfile/g" $2
+sed -i "s/xseed/$1/g" $2
+cmsRun ${2}
+if [ ! -d "$ppseos/${3}" ]; then
+    mkdir $ppseos/${3}
+fi
+rsync -avPz $outfile $ppseos/${3}/$outfile
+rm -rf *.*
