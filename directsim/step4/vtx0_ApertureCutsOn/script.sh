99cd12a34a21e86f7ac630161b48e7eb38590a8f
Gustavo <diemort@gmail.com>
2022-04-22 20:36:05 +0200

    adding step4 directsim with ApertureCuts ON

---
+++ directsim/step4/vtx0_ApertureCutsOn/script.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc900
+cmssw=$4
+scram project $cmssw
+cd $cmssw/src/
+eval `scramv1 runtime -sh`
+git cms-addpkg Validation/CTPPS
+git cms-addpkg SimTransport/PPSProtonTransport
+sed -i "/Apertures/s/False/True/g" SimTransport/PPSProtonTransport/python/OpticalFunctionsConfig_cfi.py 
+scram b -j8
+cd -
+ppseos=/eos/cms/store/group/phys_pps/sim-validation/directsim
+infile=${ppseos}/step3/step3_${1}.root
+label=${3}_${6}
+outfile=${label}_${1}.root
+sed -i "s?xinput?$infile?" $2
+sed -i "s/xfileout/$outfile/g" $2
+sed -i "s/xseed/$1/g" $2
+cmsRun ${2}
+if [ ! -d "$ppseos/${label}" ]; then
+    mkdir $ppseos/${label}
+fi
+rsync -avPz $outfile $ppseos/${label}/$outfile
+#rm -rf *.*
