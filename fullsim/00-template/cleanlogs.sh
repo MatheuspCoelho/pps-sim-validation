8d00a2a2a7fe0cd687251769ad3960a63fe3b4e3
Gustavo <diemort@gmail.com>
2024-05-02 22:51:42 +0200

    updates clean area to remove folder; closes #10

--- fullsim/00-template/cleanlogs.sh
+++ fullsim/00-template/cleanlogs.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-rm -rf err/step1/*
-rm -rf err/step2/*
-rm -rf err/step3/*
-rm -rf err/steps/*
-rm -rf out/step1/*
-rm -rf out/step2/*
-rm -rf out/step3/*
-rm -rf out/steps/*
-rm -rf log/step1/*
-rm -rf log/step2/*
-rm -rf log/step3/*
-rm -rf log/steps/*
-rm -rf err/*.*
-rm -rf out/*.*
-rm -rf log/*.*
+rm -rf err/
+rm -rf out/
+rm -rf log/
 rm -rf *.dag.*
 rm -rf silveira.cc
